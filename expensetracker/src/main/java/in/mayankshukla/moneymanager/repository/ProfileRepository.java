174ca60d73e8da95aef2acc2a99b6356c859092b
Mayank171922 <shuklamayank1702@gmail.com>
2025-09-17 10:26:40 +0530

    initial commit

---
+++ expensetracker/src/main/java/in/mayankshukla/moneymanager/repository/ProfileRepository.java
@@ -0,0 +1,15 @@
+package in.mayankshukla.moneymanager.repository;
+
+import in.mayankshukla.moneymanager.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity,Long> {
+
+    //select * from tbl_profile where email = ?
+    Optional<ProfileEntity> findByEmail(String email);
+
+    Optional<ProfileEntity> findByActivationToken(String activationToken);
+
+}
